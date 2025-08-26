48868f9ffaa8a73bb8d4c489dbc7c6c55ff12c78
NathaliaRiascos <riascosnathalia6@gmail.com>
2025-08-26 10:29:28 -0500

    feat: add portfolio section

--- src/components/sections/index.ts
+++ src/components/sections/index.ts
@@ -1,3 +1,4 @@
 export { default as Hero } from "./Hero.astro";
 export { default as About } from "./About.astro";
 export { default as Services } from "./Services.astro";
+export { default as Portfolio } from "./Portfolio.astro";
